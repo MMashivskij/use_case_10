125b9c81e9034c4c7a63a5531a724010f7fc2803
Myroslav <mmashivskyi@hirevue.com>
2023-08-12 18:37:39 +0300

    UC#10 Implement regex tests

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
 import logo from './logo.svg';
 import './App.css';
 
+import { regexChecker } from './utils/regex'; 
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <table>
+        <thead>
+          <tr>
+            <th>Test</th>
+            <th>Argument (string, min, max)</th>
+            <th>Result</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Too short input</td>
+            <td>1234, 5, 10</td>
+            <td>{regexChecker('1234', 5, 10)}</td>
+          </tr>
+          <tr>
+            <td>Too long input</td>
+            <td>12345678901, 5, 10</td>
+            <td>{regexChecker('12345678901', 5, 10)}</td>
+          </tr>
+          <tr>
+            <td>Matching input length</td>
+            <td>1aA@rrqq, 5, 10</td>
+            <td>{regexChecker('1aA@rrqq', 5, 10)}</td>
+          </tr>
+          <tr>
+            <td>Missing uppercase</td>
+            <td>1a@rrqq, 5, 10</td>
+            <td>{regexChecker('1a@rrqq', 5, 10)}</td>
+         </tr>
+          <tr>
+            <td>Missing lowercase</td>
+            <td>1@RRQQ, 5, 10</td>
+            <td>{regexChecker('1@RRQQ', 5, 10)}</td>
+          </tr>
+          <tr>
+            <td>Missing special character</td>
+            <td>122RRQQ, 5, 10</td>
+            <td>{regexChecker('122RRQQ', 5, 10)}</td>
+          </tr>
+          <tr>
+            <td>Missing digit</td>
+            <td>qwe$RQQ, 5, 10</td>
+            <td>{regexChecker('qwe$RQQ', 5, 10)}</td>
+          </tr>
+          <tr>
+            <td>Excess whitespace characters in various positions</td>
+            <td>q w e $ R Q Q, 5, 10</td>
+            <td>{regexChecker('q w e $ R Q Q', 5, 10)}</td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   );
 }
--- src/utils/regex.js
+++ src/utils/regex.js
@@ -1,5 +1,7 @@
-export const regexChecker = (string, maximumStringLength) => {
+export const regex = (string, min, max) => {
   return string.match(
-    new RegExp(`^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!"#$%&'()*+,-./:;<=>?@[\\]^_\`{|}~])[0-9a-zA-Z!@#$%^&*]{1,${maximumStringLength}}$`, 'g')
+    new RegExp(`^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!"#$%&'()*+,-./:;<=>?@[\\]^_\`{|}~])[0-9a-zA-Z!@#$%^&*]{${min},${max}}$`, 'g')
   );
 }
+
+export const regexChecker = (string, min, max) => (regex(string, min, max) ? 'match' : 'non-match')
